8a5a9f0ecb3ae8d223b0c3f18eff45cd0f45b0ca
alegottu <33403572+alegottu@users.noreply.github.com>
2022-12-10 21:51:02 -0800

    target res rather than pixel size

--- Assets/Scripts/Pixel Shader/PixelTraceManager.cs
+++ Assets/Scripts/Pixel Shader/PixelTraceManager.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class PixelTraceManager : ShaderManager
 {
-    [SerializeField] private int pixelSize = 1;
+    [SerializeField] private int targetWidth = 1;
 
     private void Awake()
     {
         ShaderSetup();
+
+        //float aspectRatio = Screen.width / Screen.height;
+        // Screen.SetResolution(targetWidth, Mathf.CeilToInt(targetWidth / aspectRatio), FullScreenMode.FullScreenWindow);
+        int pixelSize = Mathf.CeilToInt(Screen.width / targetWidth);
         shader.SetInt("PixelSize", pixelSize);
     }
 
